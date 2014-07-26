a9a3b4215c159f13b8e05e7903f94714669ce390
Kye Russell <me@kyerussell.com>
2014-07-26 11:25:23 +0800

    more admin tweaks

--- keys/admin.py
+++ keys/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from keys.models import Product, SKU, Key
 
 class KeyAdmin(admin.ModelAdmin):
-	fields = ['key', 'sku', 'key_type', 'allocated_to', 'allocated_by', 'allocated_at', 'imported_at']
+	list_display = ['key', 'sku', 'key_type', 'allocated_to', 'allocated_by', 'allocated_at', 'imported_at']
 
 class SKUAdmin(admin.ModelAdmin):
-	fields = ['name', 'product']
+	list_display = ['name', 'product']
 
 class ProductAdmin(admin.ModelAdmin):
-	fields = ['name']
+	list_display = ['name']
   
 
 admin.site.register(Key, KeyAdmin)

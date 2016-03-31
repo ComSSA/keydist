c98ccd61c849e64e0fe153bb992ca16efaddcb45
Kye Russell <kye.russell@thecut.net.au>
2016-03-31 12:07:44 +0800

    Cleaned up file.

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
+# coding=utf-8
+from __future__ import absolute_import, unicode_literals
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
-from keys.models import *
+from keys.models import SKU
+
 
 def index(request):
-	return render(request, 'home/index.html', {
-		'skus': SKU.objects.all(),
+    return render(request, 'home/index.html', {
+        'skus': SKU.objects.all(),
         'users': get_user_model().objects.all()
     })
